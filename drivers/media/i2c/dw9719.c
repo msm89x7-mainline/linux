325007760da3a819454c6b664b9d8e7f7b75a10d
Val Packett <val@packett.cool>
2025-02-17 16:07:41 -0300

    media: i2c: dw9719: Add an of_match_table
    Allow the dw9719 driver to be attached via FDT.
    
    Signed-off-by: Val Packett <val@packett.cool>

--- drivers/media/i2c/dw9719.c
+++ drivers/media/i2c/dw9719.c
@@ -421,6 +421,14 @@ static const struct i2c_device_id dw9719_id_table[] = {
 };
 MODULE_DEVICE_TABLE(i2c, dw9719_id_table);
 
+static const struct of_device_id dw9719_of_table[] = {
+	{ .compatible = "dongwoon,dw9718s", .data = (const void*)DW9718S },
+	{ .compatible = "dongwoon,dw9719",  .data = (const void*)DW9719 },
+	{ .compatible = "dongwoon,dw9761",  .data = (const void*)DW9761 },
+	{ { 0 } }
+};
+MODULE_DEVICE_TABLE(of, dw9719_of_table);
+
 static DEFINE_RUNTIME_DEV_PM_OPS(dw9719_pm_ops, dw9719_suspend, dw9719_resume,
 				 NULL);
 
@@ -428,6 +436,7 @@ static struct i2c_driver dw9719_i2c_driver = {
 	.driver = {
 		.name = "dw9719",
 		.pm = pm_sleep_ptr(&dw9719_pm_ops),
+		.of_match_table = dw9719_of_table,
 	},
 	.probe = dw9719_probe,
 	.remove = dw9719_remove,
